0b87e18dc6d8b4a52c152ecbb2692385bc08c3cc
danielmonroym <daniel.monroy@experimentality.co>
2022-02-07 09:20:00 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/udea_biosecurity_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udea_biosecurity_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:udea_biosecurity_app/screens/screens.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Productos App',
+        initialRoute: 'login',
+        routes: {'login': (_) => LoginScreen(), 'home': (_) => HomeScreen()},
+        theme: ThemeData.light()
+            .copyWith(scaffoldBackgroundColor: Colors.grey[300]));
+  }
+}
